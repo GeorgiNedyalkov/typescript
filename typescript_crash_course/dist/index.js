ec779ec07e91590080a25b4e5f7d18dd6520ac17
Flu3nt <gnedyalkov94@gmail.com>
2022-10-13 14:22:25 +0300

    Implement basic types, tuples, unions, and enums in ts

---
+++ typescript_crash_course/dist/index.js
@@ -0,0 +1,33 @@
+"use strict";
+let id = 5;
+let myName = "Georgi";
+let isGangsta = true;
+let x = 5;
+x = "Seven";
+let ids = [1, 2, 4, 5];
+let fruit = ["Apples", "Oranges", "Banana"];
+let randoms = ["Az", 56, [3, 4, 1]];
+let person = [1, "one", true];
+let employee;
+employee = [
+    [1, "John"],
+    [1, "Jill"],
+    [1, "Jimmy"],
+];
+let productId;
+productId = 23;
+productId = "23";
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 20] = "Up";
+    Direction[Direction["Down"] = 21] = "Down";
+    Direction[Direction["Left"] = 22] = "Left";
+    Direction[Direction["Right"] = 23] = "Right";
+})(Direction || (Direction = {}));
+var HeadPart;
+(function (HeadPart) {
+    HeadPart["Nose"] = "Nose";
+    HeadPart["Mouth"] = "Mouth";
+    HeadPart["Hair"] = "Hair";
+    HeadPart["Ears"] = "Ears";
+})(HeadPart || (HeadPart = {}));
